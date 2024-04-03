1041c6f81fc04188e6e2c7d84ab40bf2b809ed88
Ridgeso <nienekpalo@gmail.com>
2024-04-03 21:07:14 +0200

    Stworz instanjce aplikacji

---
+++ src/Application.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
+import java.nio.file.Path;
+
+public class Application extends JFrame
+{
+
+    public static void appMain()
+    {
+        if (instance == null)
+        {
+            new Application();
+        }
+    }
+
+    public static final Application getInstance() { return instance; }
+    public static final Path assetsDir = Path.of("assets");
+
+    private Application()
+    {
+        instance = this;
+        init();
+    }
+
+    private void setPanel(JPanel panel)
+    {
+        getContentPane().add(panel);
+        instance.revalidate();
+        instance.repaint();
+    }
+
+    private void init()
+    {
+        setTitle("DoIt");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setResizable(false);
+        setSize(1000, 630);
+        setVisible(true);
+
+        setBackground(new Color(255, 240, 206, 255));
+
+        final var iconPath = Path.of(assetsDir.toString(), "icon.jpg");
+        final var iconImage = new ImageIcon(iconPath.toString());
+        setIconImage(iconImage.getImage());
+    }
+
+    private static Application instance = null;
+}
