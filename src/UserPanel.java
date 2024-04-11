47b37fc35102d1a27243d119978824d36caa2adb
alicja832 <alicja.zosia.k@gmail.com>
2024-04-11 21:17:43 +0200

    Utworzenie panelu uzytkownika

---
+++ src/UserPanel.java
@@ -0,0 +1,82 @@
+import javax.swing.*;
+import java.awt.*;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Vector;
+import javax.swing.border.TitledBorder;
+
+public class UserPanel  extends JPanel {
+
+    public UserPanel(int Id) {
+
+        id = Id;
+        init();
+    }
+    public void init()
+    {
+        int i;
+        final var iconPath = Path.of(assetsDir.toString(), "user.jpg");
+        ImageIcon image = new ImageIcon(iconPath.toString());
+        JLabel label = new JLabel(image);
+        GridBagConstraints c = new GridBagConstraints();
+        Vector<String> header = new Vector<>();
+
+        label.setBorder(BorderFactory.createBevelBorder(1));
+        setLayout(new GridBagLayout());
+
+        data = db.getUserData(id);
+        String[] titles = {"imie:","nazwisko:","email:","numer telefonu:"};
+
+        ArrayList<JLabel> dataLabels = new ArrayList<>();
+        for (i = 0; i < data.size(); i++) {
+
+            dataLabels.add(new JLabel(titles[i]+data.get(i)));
+
+        }
+        offers = db.getUserOffers(id);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 1;
+        c.gridy = 0;
+        add(label,c);
+
+        c.gridx = 0;
+        for(i=0;i<data.size();i++)
+        {
+            c.gridy = 1+i;
+            add(dataLabels.get(i), c);
+        }
+        c.weighty = 1;
+        c.gridy = ++i;
+
+        header.add("typ");
+        header.add("miasto");
+        header.add("cena");
+        header.add("opis");
+
+        JTable table = new JTable(offers, header);
+        c.gridy = i;
+        c.gridx = 0;
+        JScrollPane pane = new JScrollPane(table);
+        pane.setBorder (BorderFactory.createTitledBorder (BorderFactory.createEtchedBorder (),
+                "Zamieszczone ogłoszenia",
+                TitledBorder.CENTER,
+                TitledBorder.TOP));
+        pane.setPreferredSize(new Dimension(500,200));
+        add(pane,c);
+
+        setBackground(new Color(255, 240, 206, 255));
+        setBorder(BorderFactory.createTitledBorder (BorderFactory.createEtchedBorder (),
+                "Panel użytkownika",
+                TitledBorder.CENTER,
+                TitledBorder.TOP));
+        setVisible(true);
+    }
+
+    private ArrayList<String> data;
+    private Vector<Vector<String>> offers;
+    private int id;
+    public static final Path assetsDir = Path.of("assets");
+    static Database db = new Database();
+
+}
